2a8e86b15bb68b5023dcc11ebe966774514e4f14
Anton Gusev <antgusev@yandex.ru>
2023-08-30 10:30:59 +0500

    add changes 1

--- hw_db_task4.py
+++ hw_db_task4.py
@@ -9,115 +9,134 @@ def create_db(conn):
 	        first_name VARCHAR(40) NOT NULL,
             last_name VARCHAR(40) NOT NULL,        
             email VARCHAR(40) NOT NULL
-        );
+            );
         """)
+        conn.commit()
                        
         cur.execute("""
         CREATE TABLE IF NOT EXISTS phones (
 	        phone_id SERIAL PRIMARY KEY,
 	        client_id INTEGER NOT NULL REFERENCES clients(client_id),
-            phone INTEGER(12)
-        );
+            phone VARCHAR(12)
+            );
         """)
+        conn.commit()
         
         cur.execute("""
         INSERT INTO clients (client_id, first_name, last_name, email)
         VALUES (0, 'None', 'None', 'None');
-        );
         """)
+        conn.commit()
 
         cur.execute("""
         INSERT INTO phones (phone_id, client_id, phone)
         VALUES (0, 0, 000000000000);
-        );
-        """)    
+        """)
+        conn.commit()    
 
-    return cur.fetchall()
+    # return cur.fetchall()
 
 def add_client(conn, first_name, last_name, email, phone=None):
     with conn.cursor() as cur:
 
         cur.execute("""
         INSERT INTO clients (client_id, first_name, last_name, email)
-        VALUES ((SELECT MAX(client_id) FROM clients)+1, 'first_name', 'last_name', 'email');
-        );
-        """)
+        VALUES ((SELECT MAX(client_id) FROM clients)+1, %s, %s, %s)
+        RETURNING client_id;
+        """, (first_name, last_name, email))
+        last_client_id = cur.fetchone()
 
         cur.execute("""
         INSERT INTO phones (phone_id, client_id, phone)
-        VALUES ((SELECT MAX(phone_id) FROM phones)+1, 'client_id', 'phone';
-        );
-        """)
+        VALUES ((SELECT MAX(phone_id) FROM phones)+1, %s, %s)
+        RETURNING phone_id, client_id, phone;
+        """, (last_client_id, phone))
+        conn.commit()
 
-    return cur.fetchall()
+    #return cur.fetchall()
 
 def add_phone(conn, client_id, phone):
     with conn.cursor() as cur:
         cur.execute("""
         INSERT INTO phones (phone_id, client_id, phone)
-        VALUES ((SELECT MAX(phone_id) FROM phones)+1,  'client_id', 'phone');
-        );
-        """)
-    return cur.fetchall()
+        VALUES ((SELECT MAX(phone_id) FROM phones)+1,  %s, %s)
+        RETURNING phone_id, client_id, phone;
+        """, (client_id, phone))
+    # return cur.fetchone()
 
-def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
+def change_client(conn, client_id, first_name=None, last_name=None, email=None, phone=None):
     with conn.cursor() as cur:
         
         cur.execute("""
         UPDATE clients
-        SET first_name = 'first_name'
-        SET last_name = 'last_name'
-        SET email = 'email' 
-        WHERE client_id = 'client_id';
-        );
-        """)
+        SET first_name = %s, last_name = %s, email = %s
+        WHERE client_id = %s
+        RETURNING client_id, first_name, last_name, email;
+        """, (first_name, last_name, email, client_id))
 
         cur.execute("""
         UPDATE phones
-        SET phone = 'phone'
-        WHERE client_id = 'client_id';
-        );
-        """)
+        SET phone = %s
+        WHERE client_id = %s
+        RETURNING phone_id, client_id, phone;
+        """, (phone, client_id))
 
-    return cur.fetchall()
+    # return cur.fetchone()
 
 def delete_phone(conn, client_id, phone):
     with conn.cursor() as cur:
         cur.execute("""
         DELETE FROM phones
-        WHERE client_id = 'client_id';
-        );
-        """)
+        WHERE client_id = %s, phone = %s;
+        """, (client_id, phone))
 
     return cur.fetchall()
 
 def delete_client(conn, client_id):
+    
+    with conn.cursor() as cur:
+        cur.execute("""
+        DELETE FROM phones
+        WHERE client_id = %s;
+        """, (client_id,))
+        conn.commit()
+    
     with conn.cursor() as cur:
         cur.execute("""
         DELETE FROM clients
-        WHERE client_id = 'client_id';
-        );
-        """)
-
-    return cur.fetchall()
+        WHERE client_id = %s;
+        """, (client_id,))
+        conn.commit()
+    # return cur.fetchall()
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     with conn.cursor() as cur:
         cur.execute("""
         SELECT c.first_name, c.last_name, c.email, c.phone FROM clients AS c
         LEFT JOIN phones AS p ON c.client_id = p.client_id
-        WHERE c.first_name LIKE "first_name" OR c.last_name LIKE "last_name" OR c.email LIKE "email" OR c.phone LIKE phone;
-        );
-        """)
-    return cur.fetchall()
+        WHERE c.first_name LIKE %s AND c.last_name LIKE %s AND c.email LIKE %s AND c.phone LIKE %s
+        RETURNING client_id, first_name, last_name, email, phone;
+        """, (first_name, last_name, email, phone))
+    return cur.fetchone()
 
 
-with psycopg2.connect(database="db_hw_4", user="postgres", password="postgres") as conn:
+with psycopg2.connect(database='db_hw_4', user='postgres', password='postgres') as conn:
+
+    # with conn.cursor() as cur:
+    #     cur.execute("""
+    #     DROP TABLE phones;
+    #     DROP TABLE clients;
+    #     """)
+    #     conn.commit()
+
+    
     print(create_db(conn))
-    print(add_client(conn, 'Иван', 'Петров', 'petrov9999@gmail.com'))
-    print(add_client(conn,'Глафира', 'Сидорова', 'sidorova9999@gmail.com', 89876543210))
-    print(add_phone(conn, 1, 899956789))
-    print(change_client(conn, 1, 'Иван', 'Петров', 'petrov9999@yandex.ru', 89687654344))
-    print(delete_phone(conn, 2))
-    print(delete_client(conn, 2))
-    print(find_client(conn, first_name=None, last_name=None, email=None, phone=None))
+    # print(add_client(conn, 'Иван', 'Петров', 'petrov9999@gmail.com'))
+    # print(add_client(conn,'Глафира', 'Сидорова', 'sidorova9999@gmail.com', 89876543210))
+    # print(add_phone(conn, 1, 899956789))
+    # print(change_client(conn, 1, 'Иван', 'Петров', 'petrov9999@yandex.ru', 89687654344))
+    # print(delete_phone(conn, 2, 89876543210))
+    # print(delete_client(conn, 2))
+    # print(find_client(conn, 'Иван', None, None, None))
+
+conn.close()
